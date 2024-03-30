01f6bc85857de6d6f356fc8ce678da9deab779b6
Arzaniel Ethan M <76161954+arzaniel@users.noreply.github.com>
2024-03-30 09:35:01 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz UI Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Quiz UI Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Nomor Kelompok:  [isi no kelompok]',
+            ),
+            const Text(
+              'Mhs 1:  [isi nim, nama]',
+            ),
+            const Text(
+              'Mhs 2:  [isi nim, nama]',
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return soalNo1();
+                  }));
+                },
+                child: const Text('   Jawaban No 1   '),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return soalNo2();
+                  }));
+                },
+                child: const Text('   Jawaban No 2   '),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /*jaawaban no 1
+  Widget soalNo1() {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: const Text("ini jawaban no 1"));
+  }*/
+
+  Widget soalNo1() {
+    return Scaffold(
+      backgroundColor: Color(0xffe2e2e2),
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/user.jpeg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'Name',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Ubah Profile',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Nama Depan',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
+            ),
+            SizedBox(height: 10),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'keith@mail.com',
+                  style: TextStyle(
+                      color: Colors.grey[400], fontSize: 18, letterSpacing: 1),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //jaawaban no 2
+  Widget soalNo2() {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: const Text("ini jawaban no 2"));
+  }
+}
